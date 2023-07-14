93148d08789694ac33509c6d2cae7f88aa02e29f
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-14 18:28:15 -0300

    colocando header na área do page content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,18 +17,41 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('welcome')
-
-            {{-- <!-- Page Heading -->
+            <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endif --}}
+            @endif
 
             <!-- Page Content -->
+            <header class="p-3 mb-3 border-bottom">
+                <div class="container">
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <a href="{{route('dashboard')}}" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+                                <img width="39" height="39" src="https://img.icons8.com/sf-black/39/FFFFFF/todo-list.png" alt="todo-list"/>
+                            </a>
+                        </div>
+                        <div class="d-flex align-items-center dropdown text-end">
+                            <div class="font-medium px-4 text-base text-gray-800">{{ Auth::user()->name }}</div>
+                            <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                            </a>
+                            <ul class="dropdown-menu text-small" style="">
+                                <li><a class="dropdown-item" href="{{route("profile.edit")}}">{{__('Profile')}}</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="{{ route("logout")}}">{{__('Log Out')}}</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </header>
+
             <main>
                 {{ $slot }}
             </main>
