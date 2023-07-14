1b23b84d91abf74dfc36f371c9d7ec9b26a544d6
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-14 18:21:31 -0300

    rota dashboard no auth, nomeando métodos index e store

--- routes/auth.php
+++ routes/auth.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -56,4 +57,14 @@ Route::middleware('auth')->group(function () {
 
     Route::get('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
+
+    Route::get('dashboard', [TaskController::class, 'index'])
+        ->name('task.index');
+
+    Route::post('dashboard', [TaskController::class, 'store'])
+        ->name('task.store');
 });
+
+Route::get('dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
