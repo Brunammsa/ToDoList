595834df3a6f616907f85b1329671e41b11d4af4
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-14 18:27:22 -0300

    adicionando form de task.store e foreach para index, que mantém erro de variável

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,40 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <x-slot name="header"></x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+    <form action="{{route('task.store')}}" method="POST">
+        @csrf
+        <div class="container text-center">
+            <div class="form-group row mt-5 justify-content-md-center align-items-center">
+                <div class="col col-lg-8">
+                    <input class="form-control" type="text" name="name" id="id" placeholder="Qual a sua próxima tarefa?">
+                </div>
+                <div class="col-md-auto">
+                    <button type="submit" class="btn btn-outline-light btn-sm">Criar</button>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="d-flex align-items-center p-1 border border-secondary-subtle border-start-0 border-end-0">
+                <div class="col-5 d-flex justify-content-start mt-2 ms-3" style="gap: 30px; color:white">
+                    <h6>#</h6>
+                    <h6>Nome</h6>
+                </div>
+
+                <div class="col-5 d-flex justify-content-end mt-2" style="gap: 30px; margin-left: 12em; color:white">
+                    <h6>Marcar como lido</h6>
+                    <h6>Editar</h6>
+                    <h6>Excluir</h6>
                 </div>
             </div>
         </div>
     </div>
+    
+    <ul class="list-group">
+        @foreach ($tasks as $task)
+            <li class="list-group-item">{{ $task->name }}</li>
+        @endforeach
+    </ul>
 </x-app-layout>
