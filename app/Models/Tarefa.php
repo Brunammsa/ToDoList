f4f198ba423f27f8726ba83bc5abd55dd8926244
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-15 14:52:24 -0300

    adicionando use softdelete

--- app/Models/Tarefa.php
+++ app/Models/Tarefa.php
@@ -4,14 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tarefa extends Model
 {
+    use SoftDeletes;
     use HasFactory;
 
     protected $fillable = [
         'name',
-        'task_done'
+        'task_done',
+        'id'
     ];
 
     protected $table = 'Tarefa';
